62b83f3911a317efab412fe330e98ddfc3b84ffb
Faustine67 <102908333+Faustine67@users.noreply.github.com>
2023-01-12 10:02:14 +0100

    Update Requêtes.sql

--- Requêtes.sql
+++ Requêtes.sql
@@ -3,7 +3,14 @@ SELECT * FROM lieu
 WHERE nom_lieu LIKE "%um";
 
 /* Nombre de personnage par lieu (trier par nombre de personnage decroissant) */ 
-
 SELECT COUNT(id_personnage) ,id_lieu
 FROM personnage
 GROUP BY id_lieu
+
+/*Nom des personnages + spécialité + adresse et lieu d'habitation, triés par lieu puis par nom
+de personnage */
+
+SELECT nom_personnage,nom_specialite,adresse_personnage,nom_lieu
+FROM personnage
+INNER JOIN specialite ON personnage.id_specialite =specialite.id_specialite
+INNER JOIN lieu ON personnage.id_lieu= lieu.id_lieu
