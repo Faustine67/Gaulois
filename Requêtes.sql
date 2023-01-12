3063204ba475836a9805cbd5dd034c9d0c319020
Faustine67 <102908333+Faustine67@users.noreply.github.com>
2023-01-12 11:10:08 +0100

    Update Requêtes.sql

--- Requêtes.sql
+++ Requêtes.sql
@@ -13,3 +13,10 @@ SELECT nom_personnage,nom_specialite,adresse_personnage,nom_lieu
 FROM personnage
 INNER JOIN specialite ON personnage.id_specialite =specialite.id_specialite
 INNER JOIN lieu ON personnage.id_lieu= lieu.id_lieu
+
+ /*Nom des spécialités avec nombre de personnages par spécialité (trié par nombre de 
+personnages décroissant). */
+SELECT nom_specialite, COUNT(id_personnage)
+FROM specialite
+INNER JOIN personnage ON specialite.id_specialite=personnage.id_specialite
+GROUP BY specialite.nom_specialite ORDER BY COUNT(id_personnage) DESC
