5c9b2488a81a81717af293b97c888eeac73037d3
Faustine67 <102908333+Faustine67@users.noreply.github.com>
2023-01-12 09:11:32 +0100

    Update Requêtes.sql

--- Requêtes.sql
+++ Requêtes.sql
@@ -2,3 +2,8 @@
 SELECT * FROM lieu
 WHERE nom_lieu LIKE "%um";
 
+/* Nombre de personnage par lieu (trier par nombre de personnage decroissant) */ 
+
+SELECT COUNT(id_personnage) ,id_lieu
+FROM personnage
+GROUP BY id_lieu
